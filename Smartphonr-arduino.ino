87c3457d990c17a8b1d0c8eaede7a8b6dbba9b3e
Achref Meghirbi <medachref.meghirbi@esprit.tn>
2019-04-15 16:47:49 +0100

    display esp8266

--- Smartphonr-arduino.ino
+++ Smartphonr-arduino.ino
@@ -6,6 +6,16 @@
 #include <GxEPD2_BW.h>
 #include <GxEPD2_3C.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
+#include <Fonts/FreeMonoBold12pt7b.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include "ArduinoJson.h"
+
+
+
 
 #if defined (ESP8266)
 // select one and adapt to your mapping, can use full buffer size (full HEIGHT)
@@ -22,7 +32,7 @@
 // 3-color e-papers
 //GxEPD2_3C<GxEPD2_154c, GxEPD2_154c::HEIGHT> display(GxEPD2_154c(/*CS=D8*/ SS, /*DC=D3*/ 0, /*RST=D4*/ 2, /*BUSY=D2*/ 4));
 //GxEPD2_3C<GxEPD2_213c, GxEPD2_213c::HEIGHT> display(GxEPD2_213c(/*CS=D8*/ SS, /*DC=D3*/ 0, /*RST=D4*/ 2, /*BUSY=D2*/ 4));
-//GxEPD2_3C<GxEPD2_290c, GxEPD2_290c::HEIGHT> display(GxEPD2_290c(/*CS=D8*/ SS, /*DC=D3*/ 0, /*RST=D4*/ 2, /*BUSY=D2*/ 4));
+//GxEPD2_3C<GxEPD2_290c, GxEPD2_290c::HEIGHT> display(GxEPD2_290c(/*CS=D8*/ 15, /*DC=D3*/ 4, /*RST=D4*/ 5, /*BUSY=D2*/ 16));
 GxEPD2_3C<GxEPD2_270c, GxEPD2_270c::HEIGHT> display(GxEPD2_270c(/*CS=D8*/ 15, /*DC=D3*/ 4, /*RST=D4*/ 5, /*BUSY=D2*/ 16));
 // can use only half buffer size
 //GxEPD2_3C<GxEPD2_420c, GxEPD2_420c::HEIGHT / 2> display(GxEPD2_420c(/*CS=D8*/ SS, /*DC=D3*/ 0, /*RST=D4*/ 2, /*BUSY=D2*/ 4));
@@ -86,6 +96,19 @@ GxEPD2_3C<GxEPD2_270c, GxEPD2_270c::HEIGHT> display(GxEPD2_270c(/*CS=D8*/ 15, /*
 //#include "GxEPD2_boards_added.h"
 //#include "GxEPD2_more_boards_added.h" // private
 
+const char* ssid = "AndroidEsprit";
+const char* password = "esprit2018";
+
+ESP8266WebServer server(80);
+IPAddress server1(192,168,1,30); // change with host ip
+IPAddress myIP;       // IP address in your local wifi net
+int count = 0;
+//HTTPClient http;
+WiFiClient client;
+
+String content ="";
+String payload="";
+
 
 
 void setup()
@@ -93,42 +116,180 @@ void setup()
   Serial.begin(115200);
   Serial.println();
   Serial.println("setup");
+  WiFi.mode(WIFI_STA);
+  Serial.println("wifi");
+
+  WiFi.begin(ssid, password);
+  // Connect to WiFi network
+  Serial.println("");
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+
+   // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.print("\r\nIP address: ");
+  Serial.println(myIP = WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
   delay(100);
   display.init(115200);
   // first update should be full refresh
-  helloWorld();
+  helloWorld("initializing...");
   display.powerOff();
   Serial.println("setup done");
 }
 
 void loop()
 {
+ synchronizeWithDataBase();
 }
 
 const char message[] = "Achref Meghirbi";
 
 
-void helloWorld()
+void helloWorld(String msg)
 {
-  //Serial.println("helloWorld");
-  display.setRotation(1);
+ display.setRotation(1);
   display.setFont(&FreeMonoBold9pt7b);
   display.setTextColor(GxEPD_BLACK);
   int16_t tbx, tby; uint16_t tbw, tbh;
-  display.getTextBounds(message, 0, 0, &tbx, &tby, &tbw, &tbh);
+  display.getTextBounds(msg, 0, 0, &tbx, &tby, &tbw, &tbh);
   uint16_t x = (display.width() - tbw) / 2;
   uint16_t y = (display.height() + tbh) / 2; // y is base line!
   display.setFullWindow();
-  display.firstPage();
-  do
-  {
+
     display.fillScreen(GxEPD_WHITE);
     display.setCursor(x, y);
-    display.print(message);
-  }
-  while (display.nextPage());
+    display.print(msg);
 
   //Serial.println("helloWorld done");
+}
+void displayMessages(String msgs[] , String users[])
+{
+    for (int i = 0 ; i< sizeof(msgs)-1 ; i++) {
+ 
+  display.setFont(&FreeMonoBold12pt7b);
+  int16_t tbx, tby; uint16_t tbw, tbh;
+  display.getTextBounds(msgs[i], 0, 0, &tbx, &tby, &tbw, &tbh);
+  uint16_t x = (display.width() - tbw) / 2;
+  uint16_t y = (display.height() + tbh) / 2; // y is base line!
+  display.setPartialWindow(0, 0, display.width(), display.height());
+  display.firstPage();
+       do
+  {
+    display.setCursor(20, y-20);
+    display.print("Msg");
+    display.print(i+1);
+    display.print(": ");
+     display.println(msgs[i]);
+      display.setCursor(x-10, y+50);
+       display.setFont(&FreeMonoBold9pt7b);
+     display.print("By: ");
+      display.print(users[i]);
+  }
+  while (display.nextPage());
+    }
+}
+
+
+void drawCornerTest()
+{
+  display.setFullWindow();
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  for (uint16_t r = 0; r <= 4; r++)
+  {
+    display.setRotation(r);
+    display.firstPage();
+    do
+    {
+      display.fillScreen(GxEPD_WHITE);
+      display.fillRect(0, 0, 8, 8, GxEPD_BLACK);
+      display.fillRect(display.width() - 18, 0, 16, 16, GxEPD_BLACK);
+      display.fillRect(display.width() - 25, display.height() - 25, 24, 24, GxEPD_BLACK);
+      display.fillRect(0, display.height() - 33, 32, 32, GxEPD_BLACK);
+      display.setCursor(display.width() / 2, display.height() / 2);
+      display.print(display.getRotation());
+    }
+    while (display.nextPage());
+    delay(2000);
+  }
+}
+
+void synchronizeWithDataBase() {
+  count = 0;
+  StaticJsonBuffer<3000> jsonBuffer;
+   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+
+    HTTPClient http;  //Declare an object of class HTTPClient
+    String globalUrl = "http://192.168.43.169:8080/api/messages/111";
+
+
+  // char * GlobalUrl = "GET http://192.168.1.30/api/achref_meghirbi/messages"; // change your personal url
+    http.begin(globalUrl);  //Specify request destination
+    int httpCode = http.GET();        
+   //Send the request
+   if (httpCode > 0) { //Check the returning code
+      payload = http.getString();   //Get the request response payload
+      JsonArray& root = jsonBuffer.parseArray(strToChar(payload));
+      Serial.println("root");
+      Serial.print("Content :");
+      int i = 0;
+      String msgs[root.size()]; 
+      String users [root.size()];
+      for ( i ; i<root.size();i++){
+         msgs[i] = root[i]["content"].as<String>();
+         JsonObject& user = root[i]["user"];
+         users[i] = user["username"].as<String>();
+         Serial.println(users[i]);  
+             
+      }
+    /*  while (count > 1) {
+          helloWorld(msgs[count]);
+          count--;
+               delay(500);
+      }
+      */
+      
+      displayMessages(msgs,users);
+   }
+  // http.end();
+}
+ 
+ 
+delay(3000);    //Send a request every 30 seconds
+  
+}
+
+
+/*char * consumeWebService(String url) {
+
+   http.begin(url);  //Specify request destination
+   int httpCode = http.GET();        
+   //Send the request
+ 
+   if (httpCode > 0) { //Check the returning code
+   payload = http.getString();   //Get the request response payload
+  // Serial.println(payload);
+   }
+   
+   //  http.end();   //Close connection
+    return strToChar(payload);
+}*/
+
+char* strToChar(String s) {
+  unsigned int bufSize = s.length() + 1; //String length + null terminator
+  char* ret = new char[bufSize];
+  s.toCharArray(ret, bufSize);
+  return ret;
 }
 
 
